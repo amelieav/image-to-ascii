0f33e080d2c18898117a8772f66b34a24e0fd5be
Amélie <ada125@student.bham.ac.uk>
2023-10-12 13:59:53 +0100

    Restructured. Simple structure, still broken workflow

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
-from imageToAscii.image_to_ascii import convert_to_ascii_art
+from image_to_ascii import convert_to_ascii_art
 
-# input_image_path = 'icon.jpg'
-input_image_path = 'digital.png'
+input_image_path = 'icon.jpg'
+# input_image_path = 'digital.png'
 # input_image_path = 'photo.png'
 
 output_text_file = 'output_art.txt'
