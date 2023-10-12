3b21a02d14f329f2e186bdb064b723316e5dad56
Amélie <ada125@student.bham.ac.uk>
2023-10-12 12:42:14 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imageToAscii',
+    version='2.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Pillow',  # Add any other dependencies here
+    ],
+)
